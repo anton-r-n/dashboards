29d287d757160d220b2a0d3c2259e779b072ab7a
Anton <anton.r7n@gmail.com>
2015-04-18 17:05:31 -0700

    Split model and view

--- scripts/lib/widget.js
+++ scripts/lib/widget.js
@@ -9,15 +9,15 @@ function Widget() {}
 /**
  * Set initial state
  */
-Widget.prototype.init = function(id, data, width) {
+Widget.prototype.init = function(id, model, width) {
   this.id = id;
-  this.data = data;
-  this.type = data.type;
-  this.data.id = this.id;
+  this.model = model;
+  this.type = model.type;
+  this.model.id = this.id;
   this.width = width;
 
-  this.process();
-  this._renderRecursively(data, this.width);
+  this.view = this.process(model);
+  this._renderRecursively(this.view, this.width);
 
   var self = this;
   setTimeout(function() {self._root = $('[data-obj="' + self.id + '"]')}, 0);
@@ -26,20 +26,22 @@ Widget.prototype.init = function(id, data, width) {
 };
 
 
-Widget.prototype.process = function() {};
+Widget.prototype.process = function(model) {
+  return model;
+};
 
 
 /**
  * Update
  */
-Widget.prototype.update = function(data, width) {
-  this.data = data;
-  this.type = data.type;
-  this.data.id = this.id;
+Widget.prototype.update = function(model, width) {
+  this.model = model;
+  this.type = model.type;
+  this.model.id = this.id;
   this.width = width;
 
-  this.process();
-  this._renderRecursively(data, this.width);
+  this.view = this.process(model);
+  this._renderRecursively(model, this.width);
   var render = $.render(this._html);
   this._root.html('');
 
@@ -66,10 +68,10 @@ Widget.prototype._renderRecursively = function(data, width) {
 
   if (data.nodes && data.nodes.length) {
     this.nodes = this._initNodes(data.nodes, width);
-    this.data.content = this._collectContent(this.nodes);
+    this.view.content = this._collectContent(this.nodes);
   }
 
-  this._html = $.tpl(data.type, this.data);
+  this._html = $.tpl(data.type, this.view);
 };
 
 
--- scripts/widgets/app.js
+++ scripts/widgets/app.js
@@ -10,8 +10,9 @@ widgets.Column = function() {};
 widgets.Column.prototype = new Widget();
 
 
-widgets.Column.prototype.process = function() {
-  this.width = this.width * this.data.width / 24;
+widgets.Column.prototype.process = function(model) {
+  this.width = this.width * model.width / 24;
+  return model;
 };
 
 
@@ -21,6 +22,13 @@ widgets.ChartLinear = function() {};
 widgets.ChartLinear.prototype = new Widget();
 
 
-widgets.ChartLinear.prototype.process = function() {
+widgets.ChartLinear.prototype.process = function(model) {
   console.log('process Linear Chart');
+
+  var margin = {'top': 10, 'right': 50, 'bottom': 20, 'left': 50},
+      geom = {'width': this.width, 'height': 200, 'margin': margin};
+
+  this.model.viewBox = [0, 0, geom.width, geom.height].join(' ');
+
+  return model;
 };
