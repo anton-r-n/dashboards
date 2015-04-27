fdf49f65d919f13a9c0aeae39c2810bb8cb7d47a
Anton <anton.r7n@gmail.com>
2015-04-26 23:58:40 -0700

    Rename methods

--- app/widgets/chart_linear/chart_linear.js
+++ app/widgets/chart_linear/chart_linear.js
@@ -14,50 +14,48 @@ widgets.ChartLinear.prototype.process = function(model) {
 
   this.margin = {'top': 5, 'right': 50, 'bottom': 20, 'left': 50};
   this.geom = {'width': this.width, 'height': 150};
+  this.chart_height = this.geom.height - this.margin.bottom - this.margin.top;
 
   view.name = model.name;
   view.viewBox = [0, 0, this.geom.width, this.geom.height].join(' ');
-  view.axes = this.buildAxes(model);
+  view.axes = this._buildAxes(model);
 
   view.data = {};
   view.data_translate = [this.margin.left, this.margin.top].join(', ');
-  var height = this.geom.height - this.margin.bottom - this.margin.top;
   for (var type in view.axes) {
     if (type in model.data) {
-      view.data[type] = this.scaleData(
-          model.data[type], view.axes[type], view.axes['bottom'], height);
+      view.data[type] = this._scaleData(
+          model.data[type], view.axes[type], view.axes['bottom']);
     }
   }
-
   return view;
 };
 
 
-widgets.ChartLinear.prototype.buildAxes = function(model) {
+widgets.ChartLinear.prototype._buildAxes = function(model) {
   var axes = {};
   for (var type in model.axes) {
     if (type in model.data) {
-      this.updateMinMax(model.data[type], model.axes[type]);
+      this._updateMinMax(model.data[type], model.axes[type]);
     }
-    var method = 'buildAxis' + type.charAt(0).toUpperCase() + type.slice(1);
+    var method = '_buildAxis' + type.charAt(0).toUpperCase() + type.slice(1);
     axes[type] = this[method](model.axes[type]);
   }
   return axes;
 };
 
 
-widgets.ChartLinear.prototype.buildAxisLeft = function(_axis) {
+widgets.ChartLinear.prototype._buildAxisLeft = function(_axis) {
   var axis = {'min': _axis.min, 'max': _axis.max, 'type': 'left'};
   var margin = this.margin,
-      width = this.geom.width - margin.left - margin.right,
-      height = this.geom.height - margin.bottom - margin.top;
+      width = this.geom.width - margin.left - margin.right;
 
   var max = Math.max(_axis.max, _axis._max * 1.05),
       min = Math.min(_axis.min, _axis._min * 1.05);
 
-  axis.scale = (max - min) / height;
+  axis.scale = (max - min) / this.chart_height;
   axis.translate = [margin.left, margin.top].join(',');
-  axis.line = {'x2': 0, 'y2': height};
+  axis.line = {'x2': 0, 'y2': this.chart_height};
   axis.tick_line = {'x2': width, 'y2': 0};
 
   axis.ticks = [];
@@ -74,17 +72,16 @@ widgets.ChartLinear.prototype.buildAxisLeft = function(_axis) {
 };
 
 
-widgets.ChartLinear.prototype.buildAxisBottom = function(_axis) {
+widgets.ChartLinear.prototype._buildAxisBottom = function(_axis) {
   var axis = {'min': _axis.start, 'type': 'bottom'};
   var margin = this.margin,
-      width = this.geom.width - margin.left - margin.right,
-      height = this.geom.height - margin.bottom - margin.top;
+      width = this.geom.width - margin.left - margin.right;
 
   axis.max = _axis.start + _axis.step * _axis.length;
   axis.scale = (axis.max - axis.min) / width;
   axis.translate = [margin.left, this.geom.height - margin.bottom].join(',');
   axis.line = {'x2': width, 'y2': 0};
-  axis.tick_line = {'x2': 0, 'y2': -height};
+  axis.tick_line = {'x2': 0, 'y2': -this.chart_height};
   axis.step = _axis.step;
 
   axis.ticks = [];
@@ -106,17 +103,16 @@ widgets.ChartLinear.prototype.buildAxisBottom = function(_axis) {
 };
 
 
-widgets.ChartLinear.prototype.scaleData = function(data, axis, x_axis, height) {
+widgets.ChartLinear.prototype._scaleData = function(data, axis, x_axis) {
   var d = [], current;
   for (var i = 0; i < data.length; i++) {
-    var curve = ['M'];
+    var curve = ['M'], x, y;
     for (var j = 0; j < data[i].length; j++) {
       current = data[i][j];
       if (typeof current === 'number') {
-        curve.push(
-            Math.round(x_axis.step * j / x_axis.scale) +
-            ',' + (height - Math.round((current - axis.min) / axis.scale))
-        );
+        x = Math.round(x_axis.step * j / x_axis.scale);
+        y = this.chart_height - Math.round((current - axis.min) / axis.scale);
+        curve.push(x + ',' + y);
       }
     }
     d.push(curve.join(' '));
@@ -125,7 +121,7 @@ widgets.ChartLinear.prototype.scaleData = function(data, axis, x_axis, height) {
 };
 
 
-widgets.ChartLinear.prototype.updateMinMax = function(data, axis) {
+widgets.ChartLinear.prototype._updateMinMax = function(data, axis) {
   var current,
       max = Number.NEGATIVE_INFINITY,
       min = Number.POSITIVE_INFINITY;
