96afb42177e073b30a95e2290d36842472ab8165
Anton <anton.r7n@gmail.com>
2015-05-02 20:16:50 -0700

    Bottom axis for Bar Chart

--- app/widgets/bar_chart/bar_chart.js
+++ app/widgets/bar_chart/bar_chart.js
@@ -21,3 +21,29 @@ widgets.BarChart.prototype.process = function(model) {
   view.axes = this._buildAxes(model);
   return view;
 };
+
+
+widgets.BarChart.prototype._buildAxisBottom = function(_axis) {
+  var axis = {'type': 'bottom'};
+  var margin = this.margin,
+      axis_width = this.geom.width - margin.left - margin.right;
+
+  axis.translate = [margin.left, this.geom.height - margin.bottom].join(',');
+  axis.line = {'x2': axis_width, 'y2': 0};
+  axis.tick_line = {'x2': 0, 'y2': -this.chart_height};
+  axis.ticks = [];
+
+  var tick_width = axis_width / _axis.cols.length;
+  for (var i = 0; i < _axis.cols.length; i++) {
+    var col = _axis.cols[i];
+    axis.ticks.push({
+      'dx': '0',
+      'dy': '1.4em',
+      'xy': [Math.round((i + .5) * tick_width), 0].join(','),
+      'val': col,
+      'str': col
+    });
+  }
+
+  return axis;
+};
